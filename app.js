2c64ef156a42df5e0c97e6c3d2be9abcf5756739
Patrick Rauls <patrickrauls@Patricks-MacBook-Air.local>
2016-12-21 11:39:24 -0700

    Implemented GET for products

--- app.js
+++ app.js
@@ -2,9 +2,20 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res)=>{
-  res.send('what it do?')
+app.get('/', (req, res) => {
+    res.send('what it do?')
 });
+app.get('/api/products', (req, res) => {
+    let products = [{
+        name: 'Javascript book',
+        price: 9.99
+    }, {
+        name: 'A Christmas Carol',
+        price: 19.99
+    }];
+    let stringBean = JSON.stringify(products);
+    res.send(stringBean);
+})
 app.listen(3000, function() {
-  console.log('Listening on port 3000. Go to http://localhost:3000/');
+    console.log('Listening on port 3000. Go to http://localhost:3000/');
 });
