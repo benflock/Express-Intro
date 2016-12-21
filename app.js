9af1d9a94e1a58c9b38d583bc7a42955d4fcb170
Patrick Rauls <patrickrauls@Patricks-MacBook-Air.local>
2016-12-21 11:57:15 -0700

    Added id search to API

--- app.js
+++ app.js
@@ -1,21 +1,32 @@
 'use strict';
 const express = require('express');
 const app = express();
+let products = [{
+    id: 1,
+    name: 'Javascript book',
+    price: 9.99
+}, {
+    id: 2,
+    name: 'A Christmas Carol',
+    price: 19.99
+}];
 
 app.get('/', (req, res) => {
     res.send('what it do?')
 });
+app.get('/api', (req, res) => {
+    res.send('what can I do for you?')
+});
 app.get('/api/products', (req, res) => {
-    let products = [{
-        name: 'Javascript book',
-        price: 9.99
-    }, {
-        name: 'A Christmas Carol',
-        price: 19.99
-    }];
     let stringBean = JSON.stringify(products);
     res.send(stringBean);
-})
+});
+app.get('/api/products/:id', (req, res) => {
+    let id = Number(req.params.id);
+    let product = products.find(x => x.id === id);
+    let stringBean = JSON.stringify(product);
+    res.send(stringBean);
+});
 app.listen(3000, function() {
     console.log('Listening on port 3000. Go to http://localhost:3000/');
 });
